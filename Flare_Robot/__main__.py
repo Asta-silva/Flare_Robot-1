365b44ef760af29b5db96063ddd8488bddbb65bc
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-07 21:47:33 +0530

    Create __main__.py

--- Flare_Robot/__main__.py
+++ Flare_Robot/__main__.py
@@ -692,7 +692,7 @@ def main():
 
     else:
         LOGGER.info("Flare Robot is deployed successfully!")
-        updater.drop_pending_updates(timeout=15, read_latency=4, clean=True)
+        updater.start_polling(timeout=15, read_latency=4, drop_pending_updates=True, allowed_updates=Update.ALL_TYPES)
 
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()
