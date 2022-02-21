0d6211162299915981d0c533451164bb3d279217
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-21 22:13:46 +0530

    Update cust_filters.py

--- Flare_Robot/modules/cust_filters.py
+++ Flare_Robot/modules/cust_filters.py
@@ -13,22 +13,22 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from zeldris import LOGGER, dispatcher
-from zeldris.modules.connection import connected
-from zeldris.modules.disable import DisableAbleCommandHandler
-from zeldris.modules.helper_funcs.alternate import send_message, typing_action
-from zeldris.modules.helper_funcs.chat_status import user_admin
-from zeldris.modules.helper_funcs.extraction import extract_text
-from zeldris.modules.helper_funcs.filters import CustomFilters
-from zeldris.modules.helper_funcs.misc import build_keyboard_parser
-from zeldris.modules.helper_funcs.msg_types import get_filter_type
-from zeldris.modules.helper_funcs.string_handling import (
+from Flare_Robot import LOGGER, dispatcher
+from Flare_Robot.modules.connection import connected
+from Flare_Robot.modules.disable import DisableAbleCommandHandler
+from Flare_Robot.modules.helper_funcs.alternate import send_message, typing_action
+from Flare_Robot.modules.helper_funcs.chat_status import user_admin
+from Flare_Robot.modules.helper_funcs.extraction import extract_text
+from Flare_Robot.modules.helper_funcs.filters import CustomFilters
+from Flare_Robot.modules.helper_funcs.misc import build_keyboard_parser
+from Flare_Robot.modules.helper_funcs.msg_types import get_filter_type
+from Flare_Robot.modules.helper_funcs.string_handling import (
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
     split_quotes,
 )
-from zeldris.modules.sql import cust_filters_sql as sql
+from Flare_Robot.modules.sql import cust_filters_sql as sql
 
 HANDLER_GROUP = 15
 
