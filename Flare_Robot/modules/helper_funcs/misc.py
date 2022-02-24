64fe9f2b99833fa7ef9c8714572a566a1f43661c
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-24 15:06:32 +0530

    Update misc.py

--- Flare_Robot/modules/helper_funcs/misc.py
+++ Flare_Robot/modules/helper_funcs/misc.py
@@ -1,9 +1,10 @@
 from math import ceil
 from typing import Dict, List
 
-from Flare_Robot import NO_LOAD
+from Flare_Robot import NO_LOAD, BOT_USERNAME
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
+
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -54,12 +55,26 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
-        pairs.append((modules[-1],))
+    if calc == 1:
+        pairs.append((modules[-1], ))
+    elif calc == 2:
+        pairs.append((modules[-1], ))
+
+    max_num_pages = ceil(len(pairs) / 10)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 8:
+        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
+             EqInlineKeyboardButton("Back", url=f"https://telegram.dog/{BOT_USERNAME}?start="),
+             EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
+
     else:
-        pairs += [[EqInlineKeyboardButton("[► Back ◄]",  callback_data="cutiepii_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="saitama_back")]]
 
     return pairs
+
 
 def send_to_list(
     bot: Bot, send_to: list, message: str, markdown=False, html=False
